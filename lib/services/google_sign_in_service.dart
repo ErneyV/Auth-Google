cce1fc037de4983449b5f8a3864a11f6076d53c4
Erney Vargas <erney.vargas@MCT.COM.CO>
2021-02-03 19:52:30 -0500

    Fin seccion 25, autenticacion en Backend

--- lib/services/google_sign_in_service.dart
+++ lib/services/google_sign_in_service.dart
@@ -1,4 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSignInService {
   static GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -11,9 +12,24 @@ class GoogleSignInService {
     try {
       final GoogleSignInAccount account = await _googleSignIn.signIn();
       final googleKey = await account.authentication;
-      print(account);
-      print('__________________ID TOKEN_____________________');
-      print(googleKey.idToken);
+      // print(account);
+      // print('__________________ID TOKEN_____________________');
+      // print(googleKey.idToken);
+      final signInWithGoogleEndpoint = Uri(
+          scheme: 'https',
+          host: 'google-signin-avanzado.herokuapp.com',
+          path: '/google');
+
+      final session = await http.post(
+        signInWithGoogleEndpoint,
+        body: {
+          'token': googleKey.idToken,
+        },
+      );
+
+      print('=============BACKEND===========');
+      print(session.body);
+
       return account;
     } catch (e) {
       print(e);
